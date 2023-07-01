bbe96a6e234bf0607ddde771bc5319fb43d01058
Christian <cyapurf@gmail.com>
2023-07-01 11:10:31 -0300

    Added DAG for loading data to PostgreSQL

--- airflow/dags/nyc_permit_data_ingestion.py
+++ airflow/dags/nyc_permit_data_ingestion.py
@@ -48,16 +48,49 @@ def get_bus_data(ti):
     results = client.get("ez4e-fazm", limit=2000)
     ti.xcom_push(key="data", value=results)
 
+def get_pg_table_columns():
+    pg_hook = PostgresHook(postgres_conn_id='postgres_default_2')
+    query = "SELECT column_name FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'nyc_bus_data' ORDER BY ordinal_position"
+    result = pg_hook.get_records(query)
+    print(result)
+
+get_columns = PythonOperator(
+    task_id="get_pg_table_columns",
+    python_callable=get_pg_table_columns,
+    dag=dag,
+)
+
 def save_data_to_csv(ti, **kwargs):
     data = ti.xcom_pull(task_ids='get_bus_data', key='data')
     df = pd.DataFrame(data)
-    file_name = os.path.join("/usr/local/airflow/dags", "nyc_bus_data.csv")
+    file_name = os.path.join("/opt/csv_data", "nyc_bus_data.csv")
     df.to_csv(file_name, index=False)
 
 def save_data_to_postgres(ti, **kwargs):
     data = ti.xcom_pull(task_ids='get_bus_data', key='data')
-    df = pd.DataFrame(data)
-    pg_hook = PostgresHook(postgres_conn_id='postgres_default')
+    df = pd.DataFrame(data).astype({'school_year': str, 
+                                   'busbreakdown_id': str, 
+                                   'run_type': str, 
+                                   'bus_no': str, 
+                                   'route_number': str, 
+                                   'reason': str, 
+                                   'schools_serviced': str, 
+                                   'occurred_on': str, 
+                                   'created_on': str, 
+                                   'boro': str, 
+                                   'bus_company_name': str, 
+                                   'number_of_students_on_the_bus': str, 
+                                   'has_contractor_notified_schools': str, 
+                                   'has_contractor_notified_parents': str, 
+                                   'have_you_alerted_opt': str, 
+                                   'informed_on': str, 
+                                   'last_updated_on': str, 
+                                   'breakdown_or_running_late': str, 
+                                   'school_age_or_prek': str, 
+                                   'how_long_delayed': str, 
+                                   'incident_number': str})
+    print("Dataframe dtypes: ", df.dtypes)
+    pg_hook = PostgresHook(postgres_conn_id='postgres_default_2')
     pg_hook.insert_rows('nyc_bus_data', df.to_dict('records'), target_fields=df.columns.tolist())
 
 get_data = PythonOperator(
@@ -87,7 +120,7 @@ create_table = PostgresOperator(
     sql="""
     CREATE TABLE IF NOT EXISTS nyc_bus_data (
         school_year TEXT,
-        busbreakdown_id INTEGER,
+        busbreakdown_id TEXT,
         run_type TEXT,
         bus_no TEXT,
         route_number TEXT,
@@ -97,7 +130,7 @@ create_table = PostgresOperator(
         created_on TEXT,
         boro TEXT,
         bus_company_name TEXT,
-        number_of_students_on_the_bus INTEGER,
+        number_of_students_on_the_bus TEXT,
         has_contractor_notified_schools TEXT,
         has_contractor_notified_parents TEXT,
         have_you_alerted_opt TEXT,
@@ -106,10 +139,10 @@ create_table = PostgresOperator(
         breakdown_or_running_late TEXT,
         school_age_or_prek TEXT,
         how_long_delayed TEXT,
-        incident_number REAL
+        incident_number TEXT
     );
     """,
     dag=dag,
 )
 
-create_conn >> get_data >> create_table >> [save_data_csv, save_data_postgres]
+create_conn >> get_data >> create_table >> get_columns >> [save_data_csv, save_data_postgres]
